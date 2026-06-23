e6a5d95ef2b8bd78c177a5d9cf4dbf5b4aee03ea
Hastings Kaliza <hastingskalizabanda5@gmail.com>
2026-06-23 21:01:36 +0200

    Connect MongoDB using Mongoose

--- src/server.mjs
+++ src/server.mjs
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/db.mjs";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+// Connect database
+connectDB();
 
 // Test route
 app.get("/", (req, res) => {
